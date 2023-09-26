8caaf812a682a40607cefe6a67648bfd153e13c1
Youngsun <sun010331@naver.com>
2023-09-26 18:06:03 +0900

    Sqlped 사용자추가

--- OcSql.sql
+++ OcSql.sql
@@ -1,3 +1,19 @@
+
+-- root 로 접속
+
+-- pes 사용자계정 생성
+create user 'pes'@'localhost' identified by '1234';
+
+
+-- pes가 사용하는 DB(Schema) 생성
+create database pesdb;
+
+-- pes에게 pesdb 권한 부여
+grant all privileges on pesdb.* to 'pes'@'localhost' with grant option;
+
+-- pes에게 파일 처리권한 부여
+grant file on *.* to 'pes'@'localhost';
+
 -- OCR 음식 테이블
 create table food (
 	no int auto_increment,
